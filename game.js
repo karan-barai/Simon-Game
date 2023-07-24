eebdcb69e78cd35fa6397b2b7aad74a6d34b1858
Karan Barai <karanbarai5228@gmail.com>
2023-07-25 02:24:52 +0530

    Update game.js

--- game.js
+++ game.js
@@ -37,7 +37,7 @@ function checkAnswer(currentLevel) {
     } else {
       playSound("wrong");
       $("body").addClass("game-over");
-      $("#level-title").text("Game Over, Press Any Key to Restart <br> Highest level - "+level );
+      $("#level-title").html("Game Over <br> Press Any Key to Restart <br> Highest level - "+level );
 
       setTimeout(function () {
         $("body").removeClass("game-over");
